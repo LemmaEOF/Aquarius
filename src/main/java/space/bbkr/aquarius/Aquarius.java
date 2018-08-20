54ea52f8c50d29282715d78c4a9b48a33a74a851
Meredith Espinosa <goclonefilms@gmail.com>
2018-08-19 18:44:26 -0700

    move out initialization stuff

--- src/main/java/space/bbkr/aquarius/Aquarius.java
+++ src/main/java/space/bbkr/aquarius/Aquarius.java
@@ -5,23 +5,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemSimpleFoiled;
 import org.dimdev.rift.listener.ItemAdder;
-import org.dimdev.riftloader.listener.InitializationListener;
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.mixin.Mixins;
 
-
-public class Aquarius implements ItemAdder, InitializationListener {
+public class Aquarius implements ItemAdder {
 
     public static final Item FLIPPERS = new ItemSimpleFoiled(new Item.Builder().group(ItemGroup.REDSTONE).rarity(EnumRarity.RARE));
 
     @Override
     public void registerItems() {
         Item.registerItem("flippers", FLIPPERS);
-    }
-
-    @Override
-    public void onInitialization() {
-        MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.pgeg.json");
     }
 }
---
+++ src/main/java/space/bbkr/aquarius/AquariusCore.java
@@ -0,0 +1,13 @@
+package space.bbkr.aquarius;
+
+import org.dimdev.riftloader.listener.InitializationListener;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+public class AquariusCore implements InitializationListener {
+    @Override
+    public void onInitialization() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.pgeg.json");
+    }
+}
