4c4af9adddb9886eb50324c612266b93317bfd47
Meredith Espinosa <goclonefilms@gmail.com>
2018-08-30 15:28:01 -0700

    introduce trident crafting and repair

--- src/main/java/space/bbkr/aquarius/Aquarius.java
+++ src/main/java/space/bbkr/aquarius/Aquarius.java
@@ -8,9 +8,11 @@ import org.dimdev.rift.listener.ItemAdder;
 public class Aquarius implements ItemAdder {
 
     public static final Item FLIPPERS = new ItemArmor(ArmorMaterial.TURTLE, EntityEquipmentSlot.FEET, new Item.Builder().group(ItemGroup.COMBAT));
+    public static final Item PRISMARINE_ROD = new Item(new Item.Builder().group(ItemGroup.MISC));
 
     @Override
     public void registerItems() {
         Item.registerItem(new ResourceLocation("aquarius:flippers"), FLIPPERS);
+        Item.registerItem(new ResourceLocation("aquarius:prismarine_rod"), PRISMARINE_ROD);
     }
 }
