0291b0f3800308bd34b986788441ebea63a8d699
Meredith Espinosa <goclonefilms@gmail.com>
2018-11-04 01:04:22 -0700

    tracker tweaking

--- src/main/java/space/bbkr/aquarius/mixins/MixinEntityTracker.java
+++ src/main/java/space/bbkr/aquarius/mixins/MixinEntityTracker.java
@@ -4,17 +4,18 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityTracker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import space.bbkr.aquarius.EntityHookshot;
 
 @Mixin(EntityTracker.class)
 public class MixinEntityTracker
 {
-    @Inject(method = "track", at = @At("RETURN"))
-    public void trackAquariusEntity(Entity entityIn, CallbackInfo ci) {
-        if (entityIn instanceof EntityHookshot) {
-            ((EntityTracker) (Object) this).track(entityIn, 160, 20, true);
+    @Redirect(method = "track(Lnet/minecraft/entity/Entity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityTracker;track(Lnet/minecraft/entity/Entity;IIZ)V", ordinal = 1))
+    public void trackHookshotOrElseTrackArrow(final EntityTracker tracker, final Entity entity, final int trackingRange, final int updateInterval, final boolean sendVelocityUpdates) {
+        if (entity instanceof EntityHookshot) {
+            tracker.track(entity, 160, 20, true);
+        } else {
+            tracker.track(entity, trackingRange, updateInterval, sendVelocityUpdates);
         }
     }
 }
